71025c999a93384726a789bd66c4b9e22b0f526f
eddiejiang <877488655@qq.com>
2023-05-17 15:46:18 +0800

    update

--- src/components/index-about.vue
+++ src/components/index-about.vue
@@ -5,7 +5,7 @@ export default {
             about_title_en: 'about us',
             about_title_zh: '关于我们',
             about_description: ['傲合发展有限公司是多渠道跨境出口的贸易公司。', '总部位于广州，在香港、深圳、宁波、美国加州等地设有办公室，拥有一批优秀的、专业的技能人才和积极向上的80/90后。', '深耕Amzaon平台，同时涉及自建站等其他平台，致力于开拓海外市场，覆盖欧美日本、新加坡、澳大利亚、印度、中东等绝大多数发达国家及地区的消费者。傲合坚持深耕供应链，通过自主研发新产品，聚焦核心品类，开发新品，打造优势品牌，不断提出产品带给用户的体验活动用户对我们品牌的认可和超预期的购物体验。'],
-            about_img: './assets/images/envir-2.jpg'
+            about_img: '/src/assets/images/envir-2.jpg'
         }
     }
 }
