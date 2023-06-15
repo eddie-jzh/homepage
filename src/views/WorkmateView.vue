8b8ef633c67beb78233af62cab86e66a6bc5a45a
eddiejiang <877488655@qq.com>
2023-06-15 16:32:30 +0800

    update workmate page images

--- src/views/WorkmateView.vue
+++ src/views/WorkmateView.vue
@@ -9,18 +9,18 @@ export default {
                 hover: ['员工风采', '汇集全体傲合人梦想，齐心积跬步，致千里之遥程', 'WITH JOINT EFFORTS, EVERY SMALL STEP COUNTS TOWARDS A LONG JOURNEY']
             },
             content: [{
-                title: '2022/11 大麦运动会',
-                description: '大麦运动会是 “黑五网一”前的全员热身趣味运动会，为迎接大促给大家加油打气，让我们以斗志昂扬的姿态，迎接市场的挑战，迎接消费者的到来，为他们提供周到的服务，更贴心的产品。再创佳绩，业绩节节高。',
+                title: '2019/10 团建活动',
+                description: '在宜人的自然风景区，回归自然，返璞归真，通过团队挑战活动陶冶，放松大家的身心，心与心的交流，让我们彼此更加贴心，在工作中更能高效协助互动。',
                 url: './images/work-img1.webp',
                 flag_hover: false
             }, {
-                title: '2021/10 生日会',
-                description: '与祖国母亲一起过生日，有国才有家，有家才有我们，祝傲合大家庭永远繁荣昌盛。',
+                title: '2019/12 跨年活动',
+                description: '新年伊始，万象更新，新目标，新征程，在跨年夜这个特别的时刻，我们在一起，用我们最好的姿态迎接新的一年。',
                 url: './images/work-img2.webp',
                 flag_hover: false
             }, {
-                title: '2021/02 新春嘉年华',
-                description: '回首2020，公司和每一位员工携手抗疫，一起度过了安全，健康的一年！起航2021，我们将同心同行，共创未来。',
+                title: '2020/11 生日会',
+                description: '生日的日子当然要大家一起庆祝，笑脸是最好的祝福，被无限的祝福包围，感受这无限的欣喜吧。',
                 url: './images/work-img3.webp',
                 flag_hover: false
             }, {
@@ -29,30 +29,40 @@ export default {
                 url: './images/work-img4.webp',
                 flag_hover: false
             }, {
-                title: '2020/11 生日会',
-                description: '生日的日子当然要大家一起庆祝，笑脸是最好的祝福，被无限的祝福包围，感受这无限的欣喜吧。',
+                title: '2021/02 新春嘉年华',
+                description: '回首2020，公司和每一位员工携手抗疫，一起度过了安全，健康的一年！起航2021，我们将同心同行，共创未来。',
                 url: './images/work-img5.webp',
                 flag_hover: false
             }, {
-                title: '2019/12 跨年活动',
-                description: '新年伊始，万象更新，新目标，新征程，在跨年夜这个特别的时刻，我们在一起，用我们最好的姿态迎接新的一年。',
+                title: '2021/10 生日会',
+                description: '与祖国母亲一起过生日，有国才有家，有家才有我们，祝傲合大家庭永远繁荣昌盛。',
                 url: './images/work-img6.webp',
                 flag_hover: false
             }, {
-                title: '2019/10 团建活动',
-                description: '在宜人的自然风景区，回归自然，返璞归真，通过团队挑战活动陶冶，放松大家的身心，心与心的交流，让我们彼此更加贴心，在工作中更能高效协助互动。',
+                title: '2022/11 大麦运动会',
+                description: '大麦运动会是 “黑五网一”前的全员热身趣味运动会，为迎接大促给大家加油打气，让我们以斗志昂扬的姿态，迎接市场的挑战，迎接消费者的到来，为他们提供周到的服务，更贴心的产品。再创佳绩，业绩节节高。',
                 url: './images/work-img7.webp',
+                flag_hover: false
+            }, {
+                title: '2023/06 端午节福利',
+                description: '端午安康，超群出“粽”大礼包，助你一“粽”当先，万水千山“粽”是情，在传统端午安康中，送出传统美食， 粽子口味有若干种，但是思念只有一"粽"，愿大家"粽"是微笑，"粽"是快乐，"粽"是喜悦，"粽"是幸福。助力大家一“粽”当先。',
+                url: './images/work-img8.webp',
                 flag_hover: false
             }],
         }
     },
     methods: {
         handleImgHover(index) {
-            this.content[index].flag_hover = !this.content[index].flag_hover
+            this.reContent[index].flag_hover = !this.reContent[index].flag_hover
         }
     },
     components: {
         indexBanner
+    },
+    computed: {
+        reContent() {
+            return this.content.reverse()
+        }
     },
     mounted() { },
 };
@@ -64,7 +74,7 @@ export default {
     <section id="workmate">
         <div class="wrap">
             <ul>
-                <li v-for="(item, index) in content" :key="index">
+                <li v-for="(item, index) in reContent" :key="index">
                     <p class="item_img">
                         <img v-lazy="item.url" alt="" :class="{ hover_img: item.flag_hover }"
                             @mouseenter="handleImgHover(index)" @mouseleave="handleImgHover(index)">
